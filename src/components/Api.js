9bb1bf969c30d07bc37ac35e1e2b42c8b30fbaba
Kevin Truong <kevintru0ng@yahoo.com>
2018-05-23 17:51:38 -0700

    looking into kracken

--- src/components/Api.js
+++ src/components/Api.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react'
 
 class Api extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isMounted: false,
+      trades: [],
+    }
+  }
+
   render() {
+    const ccxt = require('ccxt')
+    // const cors = 'https://crossorigin.me.'
+
     return (
       <div>
-        
+        {console.log(new ccxt.kraken())}
+
       </div>
     );
   }
